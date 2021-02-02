fe912bd23839b3bf76761d22c39cd9ec4c8ec833
Phawit Wattanavanichakorn <phawit.w@horganice.in.th>
2021-02-02 16:45:04 +0700

    no message

--- src/android/HelpcrunchPlugin.java
+++ src/android/HelpcrunchPlugin.java
@@ -1,23 +1,81 @@
 package com.horganice.cordova.plugin;
 // Cordova-required packages
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.helpcrunch.library.core.HelpCrunch;
+
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
 public class HelpcrunchPlugin extends CordovaPlugin {
-  @Override
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) {
-      // Verify that the user sent a 'show' action
-    //   if (!action.equals("show")) {
-    //     callbackContext.error("\"" + action + "\" is not a recognized action.");
-    //     return false;
-    //   }
-      // Send a positive result to the callbackContext
-      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
-      callbackContext.sendPluginResult(pluginResult);
-      return true;
+  private enum Action {
+    initialize {
+      @Override
+      void performAction(JSONArray args, CallbackContext callbackContext, CordovaInterface cordova) {
+        String ORGANISATION = args.optString(0);
+        Integer APP_ID = args.optInt(1);
+        String SECRET = args.optString(2);
+        Log.v("TEST",ORGANISATION);
+        Log.v("TEST", String.valueOf(APP_ID));
+        Log.v("TEST",SECRET);
+        HelpCrunch.initialize(ORGANISATION, APP_ID, SECRET);
+        callbackContext.success();
+      }
+    },
+    showChatScreen {
+      @Override
+      void performAction(JSONArray args, CallbackContext callbackContext, CordovaInterface cordova) {
+        Context context = cordova.getActivity().getApplicationContext();
+        HelpCrunch.showChatScreen(context);
+        callbackContext.success();
+      }
+    },
+    unknown {
+      @Override
+      void performAction(JSONArray args, CallbackContext callbackContext, CordovaInterface cordova) {
+        callbackContext.error("[Intercom-Cordova] ERROR: Undefined function");
+      }
+    };
+
+    abstract void performAction(JSONArray args, CallbackContext callbackContext, CordovaInterface cordova);
+
+    public static Action fromString(String actionAsString) {
+      Action action = unknown;
+
+      try {
+        action = valueOf(actionAsString);
+      } catch (NullPointerException ignored) {
+      } catch (IllegalArgumentException ignored) {
+      }
+
+      return action;
+    }
   }
+
+
+  public boolean execute(String actionString, final JSONArray args, final CallbackContext callbackContext) {
+    final Action action = Action.fromString(actionString);
+
+    cordova.getActivity().runOnUiThread(new Runnable() {
+      @Override
+      public void run() {
+        action.performAction(args, callbackContext, HelpcrunchPlugin.this.cordova);
+      }
+    });
+    return action != Action.unknown;
+  }
+
 }
